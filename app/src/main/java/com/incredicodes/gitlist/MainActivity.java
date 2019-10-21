fe339a901cbb34f3ce7220410f6cd7910087c01e
Nikhil Saxena <maverick@Nikhils-Air.Dlink>
2019-10-22 00:38:30 +0530

    Retrofit implementation done with API Interface

--- app/src/main/java/com/incredicodes/gitlist/MainActivity.java
+++ app/src/main/java/com/incredicodes/gitlist/MainActivity.java
@@ -8,6 +8,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +28,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d(TAG, "Started Okay");
 
-        initImageBitmaps();
+        // Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/typicode/demo/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()) {
+                    Log.d(TAG, "-> " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+                for (Post post: posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+
+                    Log.d(TAG, content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+
+        // initImageBitmaps();
     }
 
     private void initImageBitmaps () {
